ff475d4ba4cd88df5c3dc4bc7fe22b8510274cee
ObakengPhikiso <obakeng.phikiso@gmail.com>
2025-02-23 14:14:12 +0200

    domain config

--- apps/auth/src/app/auth/cognitoconfig.ts
+++ apps/auth/src/app/auth/cognitoconfig.ts
@@ -6,5 +6,5 @@ export class AuthConfig {
   public userPoolId: string =this.config.get<string>('UserPoolId');
   public clientId: string = this.config.get<string>('ClientId');
   public region: string = this.config.get<string>('Region');
-  public authority = 'https://moolahmate.auth.us-east-1.amazoncognito.com';
+  public authority: string = this.config.get<string>('Domain');
 }
